11589eda0ca81af7820f1fd6d5ccecde994f6452
S A T Y A M <108022443+Nittaany@users.noreply.github.com>
2023-12-15 17:38:06 +0530

    Push Project

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import messagebox  # Added this line
+from tkinter import messagebox  
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
 from datetime import datetime
